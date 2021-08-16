eed8d1c1fdc304c1aaf5df5ff086c91ad43f9b3f
Dioner <dionerweiss@gmail.com>
2021-08-15 22:38:10 -0300

    ensure FacebookApi returns facebook user info

--- tests/infra/api/facebook.spec.ts
+++ tests/infra/api/facebook.spec.ts
@@ -17,11 +17,8 @@ describe('FacebookAPI', () => {
   beforeEach(() => {
     httpClient.get
       .mockResolvedValueOnce({ access_token: 'any_app_token' })
-      .mockResolvedValueOnce({
-        data: {
-          user_id: 'any_user_id'
-        }
-      })
+      .mockResolvedValueOnce({ data: { user_id: 'any_user_id' } })
+      .mockResolvedValueOnce({ id: 'any_fb_id', name: 'any_fb_name', email: 'any_fb_email' })
     sut = new FacebookApi(httpClient, clientId, clientSecret)
   })
 
@@ -59,6 +56,16 @@ describe('FacebookAPI', () => {
         fields: 'id,name,email',
         access_token: 'any_client_token'
       }
+    })
+  })
+
+  it('should return facebook user info', async () => {
+    const fbUser = await sut.loadUser({ token: 'any_client_token' })
+
+    expect(fbUser).toEqual({
+      facebookId: 'any_fb_id',
+      name: 'any_fb_name',
+      email: 'any_fb_email'
     })
   })
 })
