3af44fc0b124501ad0ee249db838dd6c14390ac0
Dioner <dionerweiss@gmail.com>
2021-08-20 21:05:51 -0300

    apply composite

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -1,5 +1,5 @@
 import { badRequest, HttpResponse, ok, serverError, unauthorized } from '@/application/helpers'
-import { RequiredStringValidator } from '@/application/validation'
+import { RequiredStringValidator, ValidationComposite } from '@/application/validation'
 import { FacebookAuthentication } from '@/domain/features'
 import { AccessToken } from '@/domain/models'
 
@@ -33,7 +33,8 @@ export class FacebookLoginController {
   }
 
   private validate (httpRequest: HttpRequest): Error | undefined {
-    const validator = new RequiredStringValidator(httpRequest.token, 'token')
+    const validators = [new RequiredStringValidator(httpRequest.token, 'token')]
+    const validator = new ValidationComposite(validators)
     return validator.validate()
   }
 }
