9a9eb949ca38e694f04a2bae6da4818fa48433d8
Dioner <dionerweiss@gmail.com>
2021-08-25 18:58:02 -0300

    connect to postgres db before server init

--- src/main/index.ts
+++ src/main/index.ts
@@ -3,5 +3,10 @@ import { app } from '@/main/config/app'
 import { env } from '@/main/config/env'
 
 import 'reflect-metadata'
+import { createConnection } from 'typeorm'
+import { config } from '@/infra/postgres/helpers'
 
-app.listen(env.port, () => console.log(`Server running at ${env.port}`))
+createConnection(config)
+  .then(() =>
+    app.listen(env.port, () => console.log(`Server running at ${env.port}`))
+  ).catch(console.error)
