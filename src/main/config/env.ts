cd08703884827332260cd835fa8b48c850f35c09
Dioner <dionerweiss@gmail.com>
2021-10-18 14:47:00 -0300

    move sensive config to .env file

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,9 +1,9 @@
 export const env = {
   facebookApi: {
-    clientId: process.env.FB_CLIENT_ID ?? '274941864043409',
-    clientSecret: process.env.FB_CLIENT_SECRET ?? '4e23318e5fdaf7f130065312c61bfd58',
-    token: 'EAAD6DuQj45EBAJAS2kF7SwoCZBPCFCUNf3FDaBfHfQSUEiJf8XZASRyOBuStEOImhxl9POYRFinhxGGLxNPB3vTLn4OTjAIvUusnPwyzxOZCRydo5wQCIRZB0wfdD2P3rUJduDW8zNlZAhLaQBd0sMrCG2laddn1l0rUzSw879iCd1vZB6MZCn9dRHR13sciIVRZCHY7bepKkxe80FmiqhJ2'
+    clientId: process.env.FB_CLIENT_ID ?? '',
+    clientSecret: process.env.FB_CLIENT_SECRET ?? '',
+    token: process.env.FB_ACCESS_TOKEN ?? ''
   },
   port: process.env.PORT ?? 8080,
-  jwtSecret: process.env.JWT_SECRET ?? 'ijashdfiduhs'
+  jwtSecret: process.env.JWT_SECRET ?? ''
 }
