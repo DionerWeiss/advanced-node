75d7381db2411809a0ab19df9adfba2925e6d57b
Dioner <dionerweiss@gmail.com>
2021-11-09 20:42:14 -0300

    separate factories by layers

--- src/main/factories/domain/use-cases/facebook-authentication.ts
+++ src/main/factories/domain/use-cases/facebook-authentication.ts
@@ -1,7 +1,7 @@
 import { FacebookAuthentication, setupFacebookAuthentication } from '@/domain/use-cases'
-import { makeFacebookApi } from '@/main/factories/gateways/facebook-api'
-import { makeJwtTokenHandler } from '@/main/factories/gateways'
-import { makePgUserAccountRepo } from '@/main/factories/repos'
+import { makeFacebookApi } from '@/main/factories/infra/gateways/facebook-api'
+import { makeJwtTokenHandler } from '@/main/factories/infra/gateways'
+import { makePgUserAccountRepo } from '@/main/factories/infra/repos'
 
 export const makeFacebookAuthentication = (): FacebookAuthentication => {
   return setupFacebookAuthentication(
