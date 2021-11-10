f11b934980a2adeb2a4e210f77fe853354abb88c
Dioner <dionerweiss@gmail.com>
2021-11-09 22:18:47 -0300

    isolate SUT creation

--- tests/application/controllers/save-profile-picture.spec.ts
+++ tests/application/controllers/save-profile-picture.spec.ts
@@ -5,15 +5,19 @@ type HttpRequest = { file: any }
 type Model = Error
 
 class SavePictureController {
-  async handle ({ file }: HttpRequest): Promise<HttpResponse<Model> > {
+  async handle ({ file }: HttpRequest): Promise<HttpResponse<Model>> {
     return badRequest(new RequiredFieldError('file'))
   }
 }
 
 describe('SavePictureController', () => {
-  it('should return 400 if file is not provided', async () => {
-    const sut = new SavePictureController()
+  let sut: SavePictureController
 
+  beforeEach(() => {
+    sut = new SavePictureController()
+  })
+
+  it('should return 400 if file is not provided', async () => {
     const httpResponse = await sut.handle({ file: undefined })
 
     expect(httpResponse).toEqual({
@@ -23,9 +27,7 @@ describe('SavePictureController', () => {
   })
 
   it('should return 400 if file is not provided', async () => {
-    const sut = new SavePictureController()
-
-    const httpResponse = await sut.handle({ file: null })
+    const httpResponse = await sut.handle({ file: undefined })
 
     expect(httpResponse).toEqual({
       statusCode: 400,
